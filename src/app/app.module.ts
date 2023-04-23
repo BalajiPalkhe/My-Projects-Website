ba03ac4e07e0958da716a78cd6af5e6d377d98d4
Balaji <bpalkhe@gmail.com>
2023-04-23 16:13:03 +0530

    deleted unneccessary files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StringpComponent } from './stringp/stringp.component';
-import { ArraypComponent } from './arrayp/arrayp.component';
+
 import { LengthpComponent } from './lengthp/lengthp.component';
-import { TemperaturepComponent } from './temperaturep/temperaturep.component';
-import { WordpComponent } from './wordp/wordp.component';
-import { CalcpComponent } from './calcp/calcp.component';
+
 import { FormsModule } from '@angular/forms';
 import { EventComponent } from './event/event.component';
 
@@ -16,11 +14,9 @@ import { EventComponent } from './event/event.component';
   declarations: [
     AppComponent,
     StringpComponent,
-    ArraypComponent,
+
     LengthpComponent,
-    TemperaturepComponent,
-    WordpComponent,
-    CalcpComponent,
+
     EventComponent
   ],
   imports: [
